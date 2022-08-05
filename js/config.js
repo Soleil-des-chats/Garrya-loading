8265ab86ce519f6baaaf22555e0a0dd1ae78ff45
Bruhx-Homebrew <soleil-des-chats@hotmail.com>
2022-08-05 20:53:04 +0200

    Commit1

---
+++ js/config.js
@@ -0,0 +1,108 @@
+﻿/*
+--------------------------------------
+--------LEAKED BY ANONYMOUS LEAKR --------------
+----------------------------------------
+      _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.04
+	
+--------------------------------------
+--------LEAKED BY ANONYMOUS LEAKR --------------
+----------------------------------------	
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "Garrya";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 0;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/music.ogg", name: "Di Young - Pixel Cat"},
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 50;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Amusez Vous Bien",
+"Bienvenue ! Merci De Rejoindre Notre Serveur!",
+"Workshop = https://steamcommunity.com/sharedfiles/filedetails/?id=2829927377"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
+
+/*
+--------------------------------------
+--------LEAKED BY ANONYMOUS LEAKR --------------
+----------------------------------------
+*/
